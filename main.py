2fae076d154921053347e7a8c269813a0fcab9f0
WendelHime <wendelhime@hotmail.com>
2023-07-13 10:13:58 -0300

    request ntp for setting time; sending metrics to the service; updating linear regression equation for pH metric

--- main.py
+++ main.py
@@ -1,30 +1,16 @@
 import time
 import network
 
+import machine
 from machine import ADC, Pin
 import uasyncio as asyncio
+import ntptime
 
 import dht
 import dftds
 import secret
 import urequests
-
-class MetricsRequest:
-    def __init__(self, ph_value, tds_value, temperature, humidity):
-        self.ph_value = ph_value
-        self.tds_value = tds_value
-        self.ec = tds_value*2
-        self.temperature = temperature
-        self.humidity = humidity
-
-
-def collect_ph_voltages(ph_sensor):
-    ph_voltages = []
-    for i in range(10):
-        voltage = ph_sensor.read_u16()
-        ph_voltages.append(voltage)
-        time.sleep(0.1)
-    return ph_voltages
+import ujson
 
 
 HUMIDITY_POWER_PIN = 15
@@ -40,9 +26,49 @@ ph_sensor = ADC(Pin(PH_PIN))
 tds_sensor = dftds.GravityTDS(TDS_PIN, adc_range=65535, k_value_repository=dftds.KValueRepositoryFlash('tds_calibration.json'))
 tds_sensor.begin()
 
+s = machine.unique_id()
+chars = []
+for b in s:
+    h = hex(b)[2:]
+    if len(h) == 1:
+        h = '0' + h
+    chars.append(h)
+serial_id = '-'.join(chars)
+
 wlan = network.WLAN(network.STA_IF)
 wlan_status = 0
 wlan_status_lock = asyncio.Lock()
+
+# (date(2000, 1, 1) - date(1900, 1, 1)).days * 24*60*60
+# (date(1970, 1, 1) - date(1900, 1, 1)).days * 24*60*60
+NTP_DELTA = 3155673600 if time.gmtime(0)[0] == 2000 else 2208988800
+
+# The NTP host can be configured at runtime by doing: ntptime.host = 'myhost.org'
+host = "pool.ntp.org"
+
+def set_time(hrs_offset=0):  # Local time offset in hrs relative to UTC
+    NTP_QUERY = bytearray(48)
+    NTP_QUERY[0] = 0x1B
+    try:
+        addr = socket.getaddrinfo(host, 123)[0][-1]
+    except OSError as ex:
+        print(ex)
+        return 0
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    poller = select.poll()
+    poller.register(s, select.POLLIN)
+    try:
+        s.sendto(NTP_QUERY, addr)
+        if poller.poll(1000):  # time in milliseconds
+            msg = s.recv(48)
+            val = struct.unpack("!I", msg[40:44])[0]  # Can return 0
+            return max(val - NTP_DELTA + hrs_offset * 3600, 0)
+    except OSError as ex:
+        print(ex)
+        pass  # LAN error
+    finally:
+        s.close()
+    return 0  # Timeout or LAN error occurred
 
 async def connect_to_network():
     wlan.active(True)
@@ -77,39 +103,77 @@ async def blink_led():
             time.sleep(0.5)
     onboard_led.on()
 
+class MetricsRequest:
+    def __init__(self, ph_value, tds_value, temperature, humidity, serial_id):
+        self.ph = ph_value
+        self.tds = tds_value
+        self.ec = tds_value*2
+        self.temperature = temperature
+        self.humidity = humidity
+        self.sensor_id = serial_id
+        self.sensor_version = "0.0.1"
+        self.alias = "alface 1"
+        self.timestamp = time.time()
+
+    def __repr__(self):
+        return repr({"ph": self.ph, "tds": self.tds, "ec": self.ec, "temperature": self.temperature, "humidity": self.humidity, "sensor_id": self.sensor_id, "sensor_version": self.sensor_version, "alias": self.alias, "timestamp": self.timestamp})
+
+    def __getstate__(self):
+        return {"ph": self.ph, "tds": self.tds, "ec": self.ec, "temperature": self.temperature, "humidity": self.humidity, "sensor_id": self.sensor_id, "sensor_version": self.sensor_version, "alias": self.alias, "timestamp": self.timestamp}
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
+
 async def collect_metrics():
     try:
         temperature_sensor.measure()
         temperature = temperature_sensor.temperature()
         humidity = temperature_sensor.humidity()
-        print("Temperature: {}C Humidity: {:.0f}% ".format(temperature, humidity))
 
         ph_value = collect_ph()
 
         tds_sensor.temperature = temperature
         tds_value = tds_sensor.update()
-        print("TDS: {}ppm, EC: {} mS/cm".format(tds_value, tds_value*2))
 
-        request = MetricsRequest(ph_value, tds_value, temperature, humidity)
-        #urequests.post('', )
+        metrics = MetricsRequest(ph_value, tds_value, temperature, humidity, serial_id)
+        send_metrics(metrics)
     except Exception as ex:
         print("failed to acquire data", ex)
+
+def collect_ph_voltages(ph_sensor):
+    ph_voltages = []
+    for i in range(10):
+        voltage = ph_sensor.read_u16()
+        ph_voltages.append(voltage)
+        time.sleep(0.1)
+    return ph_voltages
 
 def collect_ph():
     ph_voltages = collect_ph_voltages(ph_sensor)
     avg_ph_voltage = sum(ph_voltages) / len(ph_voltages)
-    # We're providing to the pH 3V
-    ph_voltage = avg_ph_voltage * 3 / 65535
+    # We're providing to the pH 5V
+    ph_voltage = (avg_ph_voltage * 5 / 65535)
     # linear regression after calibrating ph probe
-    ph_value = -4.25373*ph_voltage+17.28164
-    print("PH Voltage: {}V, avg voltages: {}, estimated PH {} ".format(ph_voltage, avg_ph_voltage, ph_value))
+    ph_value = -4.852947599489467*ph_voltage+23.73194905699139
     return ph_value
+
+def send_metrics(metrics):
+    data = ujson.dumps(metrics.__dict__)
+    headers = {"Content-Type": "application/json", "x-api-key": secret.api_key}
+    response = urequests.post('https://hydroponics-gateway-djqh2e23.ue.gateway.dev/metrics', headers=headers, data=data)
+    if response.status_code != 200:
+        print(response.text)
+    response.close()
 
 async def main():
     print('Connecting to network...')
     asyncio.create_task(connect_to_network())
     asyncio.create_task(blink_led())
     await asyncio.sleep(5)
+    try:
+        ntptime.settime()
+    except Exception as ex:
+        print("failed to set time", ex)
 
     while True:
         print('Starting to collect metrics')
