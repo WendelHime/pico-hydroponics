c2fc6144239d1425c879c297729e438dada0b0c0
WendelHime <wendelhime@hotmail.com>
2023-07-15 16:08:41 -0300

    remove unused ntp code

--- main.py
+++ main.py
@@ -48,37 +48,6 @@ wlan = network.WLAN(network.STA_IF)
 wlan_status = 0
 wlan_status_lock = asyncio.Lock()
 
-# (date(2000, 1, 1) - date(1900, 1, 1)).days * 24*60*60
-# (date(1970, 1, 1) - date(1900, 1, 1)).days * 24*60*60
-NTP_DELTA = 3155673600 if time.gmtime(0)[0] == 2000 else 2208988800
-
-# The NTP host can be configured at runtime by doing: ntptime.host = 'myhost.org'
-host = "pool.ntp.org"
-
-def set_time(hrs_offset=0):  # Local time offset in hrs relative to UTC
-    NTP_QUERY = bytearray(48)
-    NTP_QUERY[0] = 0x1B
-    try:
-        addr = socket.getaddrinfo(host, 123)[0][-1]
-    except OSError as ex:
-        print(ex)
-        return 0
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    poller = select.poll()
-    poller.register(s, select.POLLIN)
-    try:
-        s.sendto(NTP_QUERY, addr)
-        if poller.poll(1000):  # time in milliseconds
-            msg = s.recv(48)
-            val = struct.unpack("!I", msg[40:44])[0]  # Can return 0
-            return max(val - NTP_DELTA + hrs_offset * 3600, 0)
-    except OSError as ex:
-        print(ex)
-        pass  # LAN error
-    finally:
-        s.close()
-    return 0  # Timeout or LAN error occurred
-
 async def connect_to_network():
     wlan.active(True)
     wlan.config(pm = 0xa11140)
