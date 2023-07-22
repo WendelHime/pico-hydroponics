ff032258572075604087bdd6a7961d3f0271aa86
WendelHime <wendelhime@hotmail.com>
2023-07-22 20:19:16 -0300

    feat: add HTTP server to be used during access point mode

--- api.py
+++ api.py
@@ -1,21 +1,48 @@
-import time
-import network
-
 from microdot import Microdot, Response
 
-import secret
+class APModeAPI():
 
-app = Microdot()
-Response.default_content_type = 'application/json'
+    app = Microdot()
+    Response.default_content_type = 'application/json'
 
-@app.route('/')
-def index(request):
-    return {'message':'hello world'}
+    def __init__(self, calibration_logic):
+        self.calibration_logic = calibration_logic
 
-@app.route('/shutdown')
-def shutdown(request):
-    request.app.shutdown()
-    return {'status': "service shutting down"}
+    @app.post('/wlan')
+    def credentials(self, request):
+        request_body = request.json
+        ssid = request_body['ssid']
+        password = request_body['password']
+        api_key = request_body['api_key']
+        content = 'ssid, password, api_key = "{}", "{}", "{}"\n'.format(ssid, password, api_key)
+        with open('config.py', mode='w') as f:
+            f.write(content)
+        return {'status': 'success'}, 200
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    @app.post('/tds/<path:buffer>')
+    def calibrate_tds(self, request, buffer):
+        k = self.calibration_logic.calibrate_tds(buffer)
+        if k != 1.0:
+            return {'k': k}, 200
+        return {'status': 'failed to calibrate TDS'}, 500
+
+    @app.get('/ph/<path:buffer>')
+    def read_ph(self, request, buffer):
+        buffer_voltage = self.calibration_logic.read_ph(buffer)
+        return {"ph_voltage": buffer_voltage, "buffer": buffer, "status": "ok"}, 200
+
+    @app.post('/ph/')
+    def calibrate_ph(self, request):
+        m, b = self.calibration_logic.calibrate_ph()
+        content = 'm, b = {}, {}'.format(m, b)
+        with open('config.py', mode='a') as f:
+            f.write(content)
+        return {'m': m, 'b': b}, 200
+
+    @app.post('/shutdown')
+    def shutdown(self, request):
+        request.app.shutdown()
+        return {'status': "service shutting down"}
+
+#if __name__ == '__main__':
+#    APModeAPI().app.run(debug=True)
